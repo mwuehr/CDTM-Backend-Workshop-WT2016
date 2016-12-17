49d16d25e22eadffa265edb3380d00693288d88d
Maximilian <maxiwuehr@gmail.com>
2016-12-17 09:45:33 +0100

    no message

--- src/server/server.py
+++ src/server/server.py
@@ -15,7 +15,9 @@ sys.setdefaultencoding('utf-8')
 #   - We need this, so that the front-end works properly.
 app = Flask(__name__, static_url_path='')
 
-
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
